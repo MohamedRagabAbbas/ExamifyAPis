7da9292b6fc7f98d879f5d4ad7ce160003a7a744
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-01-06 09:34:12 +0200

    add Teacher's controller

--- ExamifyApis/Controllers/TeacherController.cs
+++ ExamifyApis/Controllers/TeacherController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ExamifyApis.ModelServices;
+using ExamifyApis.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ExamifyApis.Controllers
@@ -7,5 +9,61 @@ namespace ExamifyApis.Controllers
     [ApiController]
     public class TeacherController : ControllerBase
     {
+        // Add a constructor to inject the Teacher Service
+        private readonly TeacherServices _teacherServices;
+        public TeacherController(TeacherServices teacherServices)
+        {
+            _teacherServices = teacherServices;
+        }
+        [HttpGet]
+        public IActionResult GetAllTeachers()
+        {
+            var response = _teacherServices.GetAllTeachers();
+            return Ok(response);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetTeacher(int id)
+        {
+            var response = _teacherServices.GetTeacher(id);
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public IActionResult AddTeacher([FromBody] TeacherInfo teacherInfo)
+        {
+            var response = _teacherServices.AddTeacher(teacherInfo);
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateTeacher([FromBody] TeacherInfo teacherInfo)
+        {
+            var response = _teacherServices.UpdateTeacher(teacherInfo);
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult RemoveTeacherFromCourse(int id)
+        {
+            var response = _teacherServices.RemoveTeacherFromCourse(id);
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult RemoveTeacher(int id)
+        {
+            var response = _teacherServices.DeleteTeacher(id);
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public IActionResult AddCourseToTeacher(int teacherId, int courseId)
+        {
+            var response = _teacherServices.AddTeacherToCourse(teacherId, courseId);
+            return Ok(response);
+        }
+
+
     }
 }
